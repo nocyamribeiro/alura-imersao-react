2cc974a757d912639af19af50ff2b8ddebe4349b
Maycon <nocyamribeiro@gmail.com>
2021-01-28 19:40:55 -0300

    Diminuir tempo de espera

--- pages/quiz.js
+++ pages/quiz.js
@@ -113,7 +113,7 @@ function QuestionWidget({
               onSubmit();
               setIsQuestionSubmited(false);
               setSelectedAlternative(undefined);
-            }, 3 * 1000);
+            }, 2 * 1000);
           }}
         >
           {question.alternatives.map((alternative, alternativeIndex) => {
